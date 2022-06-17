f5c9d9803fa8c5d1a725218d7b910c0175274e86
Paulo Gabriel <pgabrieldeveloper@outlook.com>
2022-06-17 07:41:08 -0300

    feature(hr-payroll): RestTemplate

---
+++ hr-payroll/src/main/java/com/gabriel/hrpayroll/Entities/Worker.java
@@ -0,0 +1,14 @@
+package com.gabriel.hrpayroll.Entities;
+
+
+import lombok.Data;
+
+
+@Data
+public class Worker {
+
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+}
--- hr-payroll/src/main/java/com/gabriel/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/gabriel/hrpayroll/services/PaymentService.java
@@ -1,12 +1,27 @@
 package com.gabriel.hrpayroll.services;
 import com.gabriel.hrpayroll.Entities.Payment;
+import com.gabriel.hrpayroll.Entities.Worker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class PaymentService {
 
+    @Value("${hr-worker-URI}")
+    private  String uri;
+    private final RestTemplate restTemplate;
     public Payment getPayment(Long workerId, int days){
-        return new Payment("Eduardo",900.0,days);
+        Map<String, String> map = new HashMap<>();
+        map.put("id", workerId.toString());
+        Worker worker = restTemplate.getForObject(uri + "/workers/{id}", Worker.class, map);
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
 }
